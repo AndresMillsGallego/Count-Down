c9d8370ecc2316b961511959ea2ec7ec0986e80b
Andres Mills Gallego <geneseewoodworking@gmail.com>
2022-02-04 18:11:56 -0800

    fine tuning the JS

--- js/app.js
+++ js/app.js
@@ -2,8 +2,10 @@
 
 const test = document.getElementById('testSite');
 const form = document.querySelector('form');
-const displayTrip = document.getElementById('tripInfo');
-let tripDetails = [];
+// const displayTrip = document.getElementById('tripInfo');
+const navBar = document.getElementById('mainNav');
+let userData = [];
+let trips = [];
 
 function getDate() {
   let now = new Date();
@@ -12,44 +14,51 @@ function getDate() {
   test.innerHTML = `${todaysDate} ${todaysTime}`;
 }
 
-function setCounter() {
-  let days = tripDetails[3];
-  let p = document.createElement('p');
-  p.textContent = days;
-  document.getElementById('counter').appendChild(p);
-}
+let Trip = function(destination, start, end, duration, days, hours, minutes, seconds) {
+  this.destination = destination;
+  this.tripStart = start;
+  this.tripEnd = end;
+  this.tripDuration = duration;
+  this.daysUntil = days;
+  this.hoursUntil = hours;
+  this.minutesUntil = minutes;
+  this.secondsUntil = seconds;
+};
+
+// function setCounter() {
+//   let days = tripDetails[3];
+//   let p = document.createElement('p');
+//   p.textContent = days;
+//   document.getElementById('counter').appendChild(p);
+// }
 
 function handleSubmit(e) {
   e.preventDefault();
   let destination = e.target.destination.value;
   let startDate = new Date(e.target.startDate.value);
   let endDate = new Date(e.target.endDate.value);
-  tripDetails.push(destination, startDate, endDate);
+  userData.push(destination, startDate, endDate);
   countDown();
 }
 
 function countDown() {
-  let destination = tripDetails[0];
+  let destination = userData[0];
+  let startDate = userData[1];
+  let endDate = userData[2];
   let today = new Date().getTime();
-  let startDiff = adjustTimeZone(tripDetails[1]);
-  console.log(startDiff);
-  let tripStart = tripDetails[1].getTime() + startDiff;
-  let endDiff = adjustTimeZone(tripDetails[2]);
-  let tripEnd = tripDetails[2].getTime() + endDiff;
-  let daysUntilTrip = Math.floor((tripStart - today)/1000/60/60/24);
-  let tripDuration = Math.floor((tripEnd - tripStart)/1000/60/60/24);
-  tripDetails.push(daysUntilTrip, tripDuration);
-  let message = `Congratulations!  You are travelling to ${destination}!  Your trip begins in ${daysUntilTrip} days.  You will be spending ${tripDuration} glorious days there!`;
-  renderMessage(message);
-  packItems(tripDetails, 'details');
-  packItems(message, 'message');
-  setCounter();
-  console.log(today);
-  console.log(tripDetails);
-}
-
-function renderMessage(message) {
-  displayTrip.innerHTML = message;
+  let startDiff = adjustTimeZone(startDate);
+  let tripStart = startDate.getTime() + startDiff;
+  let endDiff = adjustTimeZone(endDate);
+  let tripEnd = endDate.getTime() + endDiff;
+  let daysUntil = Math.floor((tripStart - today) / (1000*60*60*24));
+  let hoursUntil = Math.floor(((tripStart - today) % (1000*60*60*24)) / (1000*60*60));
+  let minutesUntil = Math.floor(((tripStart - today) % (1000*60*60)) / (1000*60));
+  let secondsUntil = Math.floor(((tripStart - today) % (1000*60)) / 1000);
+  let tripDuration = Math.floor((tripEnd - tripStart) / (1000*60*60*24));
+  let newTrip = new Trip(destination, startDate, endDate, tripDuration, daysUntil, hoursUntil, minutesUntil, secondsUntil);
+  trips.push(newTrip);
+  packItems(trips, destination);
+  console.log(trips);
 }
 
 function adjustTimeZone(date) {
@@ -61,20 +70,27 @@ function packItems(items, key) {
   localStorage.setItem(key, stringyItems);
 }
 
-function unpackItems() {
-  let unpackedDeets = localStorage.getItem('details');
-  if (unpackedDeets) {
-    let parsedDeets = JSON.parse(unpackedDeets);
-    tripDetails = parsedDeets;
-    setCounter();
-  }
-  let unpackedMessage = localStorage.getItem('message');
-  if (unpackedMessage) {
-    let parsedMessage = JSON.parse(unpackedMessage);
-    renderMessage(parsedMessage);
+// function unpackItems() {
+  //   let unpackedDeets = localStorage.getItem('details');
+//   if (unpackedDeets) {
+//     let parsedDeets = JSON.parse(unpackedDeets);
+//     tripDetails = parsedDeets;
+//     setCounter();
+//   }
+// }
+
+function handleClick(event) {
+  if (event.target.id === 'clearTrip') {
+    localStorage.removeItem('details');
+    location.reload();
+  } else if (event.target.id === 'addTrip') {
+    form.className = 'visibleForm';
   }
 }
-unpackItems();
-console.log(tripDetails);
-setInterval(getDate, 1000);
+
+// unpackItems();
+
+setInterval(getDate, countDown, 1000);
+
 form.addEventListener('submit', handleSubmit);
+navBar.addEventListener('click', handleClick);
