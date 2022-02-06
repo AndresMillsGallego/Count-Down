eb460d0f6cac4c558f607ad8692c30563a57ba9f
Andres Mills Gallego <geneseewoodworking@gmail.com>
2022-02-05 17:31:00 -0800

    got the countdown to work and render on page load

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 'use strict';
 
+// Globals go here
 const test = document.getElementById('testSite');
 const form = document.querySelector('form');
-// const displayTrip = document.getElementById('tripInfo');
+const displayTrip = document.getElementById('displayCountdown');
 const navBar = document.getElementById('mainNav');
+const destinationHeader = document.getElementById('destinationHeader');
 let userData = [];
 let trips = [];
 
+//This function gets todays date and time
 function getDate() {
   let now = new Date();
   let todaysDate = now.toDateString();
@@ -14,15 +17,12 @@ function getDate() {
   test.innerHTML = `${todaysDate} ${todaysTime}`;
 }
 
-let Trip = function(destination, start, end, duration, days, hours, minutes, seconds) {
+// Constructor function to make the trip.  Add more in the future?
+let Trip = function(destination, start, end, duration) {
   this.destination = destination;
   this.tripStart = start;
   this.tripEnd = end;
   this.tripDuration = duration;
-  this.daysUntil = days;
-  this.hoursUntil = hours;
-  this.minutesUntil = minutes;
-  this.secondsUntil = seconds;
 };
 
 // function setCounter() {
@@ -32,53 +32,85 @@ let Trip = function(destination, start, end, duration, days, hours, minutes, sec
 //   document.getElementById('counter').appendChild(p);
 // }
 
+// This takes in the user data to collect destination, trip start and trip end.
 function handleSubmit(e) {
   e.preventDefault();
   let destination = e.target.destination.value;
   let startDate = new Date(e.target.startDate.value);
   let endDate = new Date(e.target.endDate.value);
   userData.push(destination, startDate, endDate);
-  countDown();
+  makeTrip();
 }
 
-function countDown() {
+//This takes in the user data to make a new instance of the Trip object.
+function makeTrip() {
   let destination = userData[0];
   let startDate = userData[1];
   let endDate = userData[2];
-  let today = new Date().getTime();
-  let startDiff = adjustTimeZone(startDate);
-  let tripStart = startDate.getTime() + startDiff;
-  let endDiff = adjustTimeZone(endDate);
-  let tripEnd = endDate.getTime() + endDiff;
-  let daysUntil = Math.floor((tripStart - today) / (1000*60*60*24));
-  let hoursUntil = Math.floor(((tripStart - today) % (1000*60*60*24)) / (1000*60*60));
-  let minutesUntil = Math.floor(((tripStart - today) % (1000*60*60)) / (1000*60));
-  let secondsUntil = Math.floor(((tripStart - today) % (1000*60)) / 1000);
-  let tripDuration = Math.floor((tripEnd - tripStart) / (1000*60*60*24));
-  let newTrip = new Trip(destination, startDate, endDate, tripDuration, daysUntil, hoursUntil, minutesUntil, secondsUntil);
+  let tripDuration = Math.floor((endDate.getTime() - startDate.getTime()) / (1000*60*60*24));
+  let newTrip = new Trip(destination, startDate, endDate, tripDuration);
   trips.push(newTrip);
   packItems(trips, destination);
   console.log(trips);
 }
 
+// This is the code that actually counts down to the day you leave on the trip!
+function countDown() {
+  displayTrip.innerHTML = '';
+  destinationHeader.innerHTML = '';
+  let today = new Date().getTime();
+  let tripBegins = trips[0].tripStart;
+  let startDate = new Date(tripBegins);
+  let startDiff = adjustTimeZone(startDate);
+  let tripStart = startDate.getTime() + startDiff;
+  let daysUntil = Math.floor((tripStart - today) / (1000*60*60*24));
+  let hoursUntil = Math.floor(((tripStart - today) % (1000*60*60*24)) / (1000*60*60));
+  let minutesUntil = Math.floor(((tripStart - today) % (1000*60*60)) / (1000*60));
+  let secondsUntil = Math.floor(((tripStart - today) % (1000*60)) / 1000);
+  renderCountDown(daysUntil, hoursUntil, minutesUntil, secondsUntil);
+}
+
+// This renders the days, hours, minutes and seconds till the trip.  It is called in the countDown function so that it constantly renders as it is updated.
+function renderCountDown(days, hours, minutes, seconds) {
+  let h2 = document.createElement('h2');
+  h2.textContent = trips[0].destination + '!';
+  destinationHeader.appendChild(h2);
+  let dayDiv = document.createElement('div');
+  dayDiv.textContent = days + 'd';
+  let hourDiv = document.createElement('div');
+  hourDiv.textContent = hours + 'h';
+  let minuteDiv = document.createElement('div');
+  minuteDiv.textContent = minutes + 'm';
+  let secondDiv = document.createElement('div');
+  secondDiv.textContent = seconds + 's';
+  displayTrip.appendChild(dayDiv);
+  displayTrip.appendChild(hourDiv);
+  displayTrip.appendChild(minuteDiv);
+  displayTrip.appendChild(secondDiv);
+}
+
+//Function to adjust to current time zone from UTC time.
 function adjustTimeZone(date) {
   return date.getTimezoneOffset() * 60 * 1000;
 }
 
+//Packs things away to localStorage.  Items are for the items to pack, key is for the key.
 function packItems(items, key) {
   let stringyItems = JSON.stringify(items);
   localStorage.setItem(key, stringyItems);
 }
 
-// function unpackItems() {
-  //   let unpackedDeets = localStorage.getItem('details');
-//   if (unpackedDeets) {
-//     let parsedDeets = JSON.parse(unpackedDeets);
-//     tripDetails = parsedDeets;
-//     setCounter();
-//   }
-// }
+// Unpacks the data for the trip.
+function unpackItems(key) {
+  let unpackedDeets = localStorage.getItem(key);
+  if (unpackedDeets) {
+    let parsedDeets = JSON.parse(unpackedDeets);
+    trips = parsedDeets;
+    // setCounter();
+  }
+}
 
+// Simple call back function to add/remove trip.
 function handleClick(event) {
   if (event.target.id === 'clearTrip') {
     localStorage.removeItem('details');
@@ -88,9 +120,12 @@ function handleClick(event) {
   }
 }
 
-// unpackItems();
+// Event listeners and executable code go here.
+unpackItems('Santa Marta');
+countDown();
 
-setInterval(getDate, countDown, 1000);
+setInterval(getDate, 1000);
+setInterval(countDown, 1000);
 
 form.addEventListener('submit', handleSubmit);
 navBar.addEventListener('click', handleClick);
